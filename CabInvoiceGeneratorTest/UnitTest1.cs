66fba017dc716cdd1c6a86cac143d5a86ceabca9
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-03 17:31:26 +0530

    feat:Added: Invoice Summary for Multiple Rides

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -23,5 +23,20 @@ namespace CabInvoiceGeneratorTest
             //Asserting Values
             Assert.AreEqual(expected, fare);
         }
+
+        [Test]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            //Creating Instance of Invoice Generator  For Normal Ride
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) } ;
+
+            //Generating Summary for Rides
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+
+            //Asserting Values
+            Assert.AreEqual(expectedSummary, invoiceSummary);
+        }
     }
 }
